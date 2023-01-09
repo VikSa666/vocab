cc1c61776b402ff923d560336cc1d0e72d970144
victor-at-wiris <victor.santiago@wiris.com>
2023-01-09 08:28:48 +0100

    feat: first implementation of the vocabulary parsing json

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,83 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <h1>Hello World</h1>
+    <div class="cathegory-picker">
+      <button @click="setAllCathegories">All Cathegories</button>
+      <button @click="setOneCathegory">One Cathegories</button>
+      <button @click="setSelectedCathegories">Selected Cathegories</button>
+    </div>
+    <AllWords :list="wordsList" v-if="matchesAll()" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import json from "./assets/vocab.json";
+import AllWords from "./components/exams/AllCathegories.vue";
 import { foo } from "@/utils";
 
+enum Modality {
+  All,
+  One,
+  Selection,
+}
+
 export default defineComponent({
-  name: "App",
+  data() {
+    return {
+      json: json,
+      cathegoryList: [] as string[],
+      modality: Modality.All,
+      cathegorySelected: "" as string,
+      cathegoriesSelected: [] as string[],
+      wordsList: [] as { russian: string; spanish: string[] }[],
+    };
+  },
   methods: {
-    executeFoo() {
-      foo();
+    cathegories() {
+      this.json.forEach((element) => {
+        this.cathegoryList.push(element.name);
+      });
+    },
+    matchesAll(): boolean {
+      return this.modality === Modality.All;
+    },
+    matchesOne(): boolean {
+      return this.modality === Modality.One;
+    },
+    matchesSelection(): boolean {
+      return this.modality === Modality.Selection;
+    },
+    setAllCathegories() {
+      this.modality = Modality.All;
+    },
+    setOneCathegory() {
+      this.modality = Modality.One;
+      //   this.cathegorySelected = cathegory;
+    },
+    setSelectedCathegories() {
+      this.modality = Modality.Selection;
+      //   this.cathegoriesSelected = cathegories;
+    },
+    loadAllWords() {
+      this.json.forEach((element) => {
+        element.words.forEach((word) => this.wordsList.push(word));
+      });
     },
   },
   components: {
-    HelloWorld,
+    AllWords,
   },
 });
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
 </style>
---
+++ src/components/exams/AllCathegories.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="all-words">
+    <div v-for="item in myJson" :key="item.name">
+      <!-- <h2>{{ item.name }}</h2> -->
+      <div v-for="word in item.words" :key="word.russian">
+        <p>{{ word.russian }}:{{ word.spanish }}</p>
+        <input type="text" v-model="inputs[word.russian]" />
+        <button @click="confirmAnswer(word.russian)">Check</button>
+        <div>
+          Your answer was {{ confirmedAnswer }} and the actual answer was
+          {{ word.spanish }}. Therefore, you are {{ correct }}.
+        </div>
+        <button @click="i++">Next</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AllCathegories",
+  data() {
+    return {
+      correct: false,
+      answer: "",
+      confirmedAnswer: "",
+      inputs: {} as { [key: string]: string },
+      i: 0,
+    };
+  },
+  props: {
+    list: Object,
+  },
+  methods: {
+    readWord(event: any) {
+      this.answer = event.target.value;
+    },
+    confirmAnswer(russian: string) {
+      let input = this.inputs[russian];
+      this.correct = input === russian;
+    },
+    loadWords() {
+      let words = [] as { [key: string]: string }[];
+      this.list?.forEach;
+    },
+  },
+  mounted() {
+    this.loadWords();
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
