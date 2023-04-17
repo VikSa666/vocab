66da688a31e907d9c039b6868198f8f5d3c3bc1f
victor-at-wiris <victor.santiago@wiris.com>
2023-04-17 11:44:53 +0200

    chore: create component to display lists for each user

--- src/components/SecretPage.vue
+++ src/components/SecretPage.vue
@@ -6,16 +6,35 @@
   <div v-if="isAuthenticated && userData !== null">
     <h2>Welcome, {{ userData?.userName }}</h2>
     <p>Email: {{ userData?.email }}</p>
+    <form @submit.prevent="addList">
+      <span>Name: </span
+      ><input type="text" v-model="name" placeholder="name of the list" />
+      <button type="submit">Create</button>
+    </form>
+    <h2>Lists</h2>
+    <ul>
+      <li v-for="list in lists" :key="list.name">
+        <VocabularyList :list="list" />
+      </li>
+    </ul>
   </div>
   <div v-else>Loading...</div>
 </template>
 <script lang="ts" setup>
 import { ref } from "vue";
 import { useAuthStore } from "../stores/auth";
+import VocabularyList from "./VocabularyList.vue";
 
 const authStore = useAuthStore();
 const isAuthenticated = authStore.isAuthenticated;
 const userData = authStore.getUserData;
-console.log(userData);
+
+const lists = authStore.getAllVocabularyLists;
+
+const name = ref("");
+
+const addList = () => {
+  authStore.createList(name.value);
+};
 </script>
 <style lang="scss"></style>
---
+++ src/components/VocabularyList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h3>{{ list.name }}</h3>
+    <form @submit.prevent="addWord">
+      <span>Word: </span>
+      <input type="text" v-model="word" placeholder="add word" />
+      <span>Translation: </span>
+      <input type="text" v-model="translation" placeholder="translation" />
+      <button type="submit">Submit</button>
+    </form>
+    <ul>
+      <li v-for="item in list.items" :key="item.word">
+        <span>{{ item }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps, ref } from "vue";
+import { useAuthStore } from "../stores/auth";
+
+const authStore = useAuthStore();
+
+// TODO: Make common
+interface FakeVocabularyList {
+  name: string;
+  items: Array<FakeVocabularyItem>;
+}
+interface FakeVocabularyItem {
+  word: string;
+  translation: string;
+}
+
+interface Props {
+  list: FakeVocabularyList;
+}
+
+const props = defineProps<Props>();
+
+const word = ref("");
+const translation = ref("");
+const addWord = () => {
+  authStore.createWord(props.list.name, word.value, translation.value);
+};
+</script>
